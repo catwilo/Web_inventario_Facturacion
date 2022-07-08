2daa1a6614ce3e82b3a270f38fab8b3f7568ff81
catwilo <camylousuga1@gmail.com>
2022-07-07 23:40:09 -0500

    El boton de editar llama la info del producto al form de agregar

--- addConn.php
+++ addConn.php
@@ -3,14 +3,14 @@ session_start();
 require 'dbconexion.php';
 if (isset($_POST['codigoBarras'])) {
 
-    $name = $_POST['nombre'];
-    $mark = $_POST['marca'];
-    $available = $_POST['disponible'];
-    $v_unitario = $_POST['valorUnitario'];
-    $cod_barr = $_POST['codigoBarras'];
+    $name = mysqli_real_escape_string($conexion, $_POST['nombre']);
+    $mark = mysqli_real_escape_string($conexion, $_POST['marca']);
+    $available = mysqli_real_escape_string($conexion, $_POST['disponible']);
+    $v_unitario = mysqli_real_escape_string($conexion, $_POST['valorUnitario']);
+    $cod_barr = mysqli_real_escape_string($conexion, $_POST['codigoBarras']);
 
     $query = "INSERT INTO `Productos`(`nombre`, `marca`, `disponible`, `valor_unitario`, `codigo_barras`) VALUES ('$name','$mark','$available','$v_unitario','$cod_barr')";
-    $query_run=mysqli_query($conexion, $query);
+    $query_run = mysqli_query($conexion, $query);
 
     if ($query_run) {
         $_SESSION['message'] = "  Producto agregado correctamente.";
@@ -24,5 +24,5 @@ if (isset($_POST['codigoBarras'])) {
 } else {
     $_SESSION['message'] = "  Por favor llene todos los datos requeridos";
     header("Location: stock.php");
-    exit(0);}
-?>
+    exit(0);
+}
--- stock.php
+++ stock.php
@@ -56,35 +56,78 @@ require 'dbconexion.php';
             <div class="card">
                 <div class="card-header" id="headingOne">
                     <h5 class="mb-0">
-                        <button class="btn btn-outline-success col-sm-12" type="button" data-toggle="collapse" data-target="#collapseInsertar" aria-expanded="true" aria-controls="collapseInsertar">
+                        <button class="btn btn-outline-primary col-sm-12" type="button" data-toggle="collapse" data-target="#collapseInsertar" aria-expanded="true" aria-controls="collapseInsertar">
                             Insertar un nuevo Producto
                         </button>
                     </h5>
                 </div>
-                <div id="collapseInsertar" class="collapse show" aria-labelledby="headingOne" data-parent="#acordionTarjeta">
-                    <div class="card-body">
-                        <form class="form-row" action="addConn.php" method="post">
-                            <div class="form-group col-sm-3">
-                                <input type="text" class="form-control" placeholder="Nombre" name="nombre">
+
+                <?php
+                if (isset($_GET['codigo_barras'])) {
+                    $c_bar = mysqli_real_escape_string($conexion, $_GET['codigo_barras']);
+                    $query = "SELECT * FROM Productos WHERE codigo_barras='$c_bar'";
+                    $query_run = mysqli_query($conexion, $query);
+                    if (mysqli_num_rows($query_run) > 0) {
+                        $producto = mysqli_fetch_array($query_run);
+                ?>
+                        <div id="collapseInsertar" class="collapse show" aria-labelledby="headingOne" data-parent="#acordionTarjeta">
+                            <div class="card-body">
+                                <form class="form-row" action="addConn.php" method="post">
+                                    <div class="form-group col-sm-3">
+                                        <input type="text" class="form-control" value="<?= $producto['nombre']; ?>" name="nombre">
+                                    </div>
+                                    <div class="form-group col-sm-3">
+                                        <input type="text" class="form-control" value="<?= $producto['marca']; ?>" name="marca">
+                                    </div>
+                                    <div class="form-group col-sm-1">
+                                        <input type="text" class="form-control" value="<?= $producto['disponible']; ?>" name="disponible">
+                                    </div>
+                                    <div class="form-group col-sm-2">
+                                        <input type="text" class="form-control" value="<?= $producto['valor_unitario']; ?>" name="valorUnitario">
+                                    </div>
+                                    <div class="form-group col-sm-2">
+                                        <input type="text" class="form-control" value="<?= $producto['codigo_barras']; ?>" name="codigoBarras">
+                                    </div>
+                                    <button type="submit" class="btn btn-lg btn-outline-success col-sm-1 botonAgregar">
+                                        <span class="oi oi-plus"></span>
+                                    </button>
+                                </form>
                             </div>
-                            <div class="form-group col-sm-3">
-                                <input type="text" class="form-control" placeholder="Marca" name="marca">
-                            </div>
-                            <div class="form-group col-sm-1">
-                                <input type="text" class="form-control" placeholder="Disponible" name="disponible">
-                            </div>
-                            <div class="form-group col-sm-2">
-                                <input type="text" class="form-control" placeholder="Valor_Unit" name="valorUnitario">
-                            </div>
-                            <div class="form-group col-sm-2">
-                                <input type="text" class="form-control" placeholder="codigo_barras" name="codigoBarras">
-                            </div>
-                            <button type="submit" class="btn btn-outline-primary  col-sm-1">
-                                Agregar
-                            </button>
-                        </form>
+                        </div>
+                    <?php
+                    } else {
+                        echo "<h4>Ningun producto registrado con el codigo de barras registrado</h4>";
+                    }
+                } else {
+                    ?>
+                    <div id="collapseInsertar" class="collapse" aria-labelledby="headingOne" data-parent="#acordionTarjeta">
+                        <div class="card-body">
+                            <form class="form-row" action="addConn.php" method="post">
+                                <div class="form-group col-sm-3">
+                                    <input type="text" class="form-control" placeholder="Nombre" name="nombre">
+                                </div>
+                                <div class="form-group col-sm-3">
+                                    <input type="text" class="form-control" placeholder="Marca" name="marca">
+                                </div>
+                                <div class="form-group col-sm-1">
+                                    <input type="text" class="form-control" placeholder="Disponible" name="disponible">
+                                </div>
+                                <div class="form-group col-sm-2">
+                                    <input type="text" class="form-control" placeholder="Valor_Unit" name="valorUnitario">
+                                </div>
+                                <div class="form-group col-sm-2">
+                                    <input type="text" class="form-control" placeholder="codigo_barras" name="codigoBarras">
+                                </div>
+                                <button type="submit" class="btn btn-lg btn-outline-success col-sm-1 botonAgregar">
+                                    <span class="oi oi-plus"></span>
+                                </button>
+                            </form>
+                        </div>
                     </div>
-                </div>
+                <?php
+                }
+                ?>
+
             </div>
         </div>
         <div class="table-responsive" id="tabla">
@@ -102,8 +145,7 @@ require 'dbconexion.php';
                     $query = "SELECT * FROM Productos";
                     $query_run = mysqli_query($conexion, $query);
                     if (mysqli_num_rows($query_run) > 0) {
-                        foreach ($query_run as $item) {
-                    ?>
+                        foreach ($query_run as $item) { ?>
                             <tr>
                                 <td><?= $item['nombre']; ?></td>
                                 <td><?= $item['marca']; ?></td>
@@ -111,11 +153,11 @@ require 'dbconexion.php';
                                 <td><?= $item['valor_unitario']; ?></td>
                                 <td><?= $item['codigo_barras']; ?></td>
                                 <td class='col-sm-1'>
-                                    <a class="btn btn-success" href="updtConn.php?codigo_barras=<?= $item['codigo_barras']; ?>">
+                                    <a class="btn btn-success" href="stock.php?codigo_barras=<?= $item['codigo_barras']; ?>">
                                         <span class="oi oi-pencil"></span>
                                     </a>
                                     <a class="btn btn-danger" href="">
-                                        <span class="oi  oi-delete"></span>
+                                        <span class="oi oi-trash"></span>
                                     </a>
                                 </td>
                             </tr>
