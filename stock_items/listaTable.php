189582c4cce129ab26fb71c4a451f3d861616979
catwilo <camylousuga1@gmail.com>
2022-07-09 21:15:00 -0500

    Funciona la busqueda por columna con el nombre por defecto cuando no especifica )busca dato exacto(

--- stock_items/listaTable.php
+++ stock_items/listaTable.php
@@ -10,8 +10,40 @@
         </thead>
         <tbody>
             <?php
-            if ($_GET['dato']) {
-                $query = "SELECT * FROM `Productos`";
+            if ($_GET['dato'] != '') {
+                $dato = $_GET['dato'];
+                $columna = $_GET['columna'];
+                if ($columna == '') {
+                    $query = "SELECT * FROM `Productos` WHERE `nombre`='$dato'";
+                } else {
+                    $query = "SELECT * FROM `Productos` WHERE `$columna`='$dato'";
+                }
+
+                $query_run = mysqli_query($conexion, $query);
+                if (mysqli_num_rows($query_run) > 0) {
+                    foreach ($query_run as $item) { ?>
+                        <tr>
+                            <td><?= $item['nombre']; ?></td>
+                            <td><?= $item['marca']; ?></td>
+                            <td><?= $item['disponible']; ?></td>
+                            <td><?= $item['valor_unitario']; ?></td>
+                            <td><?= $item['codigo_barras']; ?>
+                            </td>
+                            <td class='col-sm-4' style="text-align: right;">
+                                <a class="btn btn-success" href="stock.php?id=<?= $item['id']; ?>">
+                                    <span class="oi oi-pencil"></span>
+                                </a>
+                                <input type="hidden" class="idDelValue" value="<?= $item['id']; ?>">
+                                <a href="javascript:void(0)" class="btn btn-outline-danger borrarBtnAjax">
+                                    <span class="oi oi-trash"></span>
+                                </a>
+                            </td>
+                        </tr>
+                    <?php
+                    }
+                } else {
+                    echo "<td><h5> NO SE ENCONTRARON REGISTROS </h5></td><td></td><td></td><td></td><td></td><td></td>";
+                }
             } else {
                 $query = "SELECT * FROM `Productos`";
                 $query_run = mysqli_query($conexion, $query);
--- stock_items/searchBar.php
+++ stock_items/searchBar.php
@@ -1,23 +1,14 @@
 <form method="GET" action="stock.php">
     <div class="d-flex align-items-stretch ">
 
-        <input name="dato" type="text" class="col-sm-8 mr-1" placeholder="Buscar productos" required="required" oninvalid="InvalidMsg(this);" oninput="InvalidMsg(this);">
+        <input name="dato" type="text" class="col-sm-8 mr-1" placeholder="Buscar productos">
 
         <select name="columna" class="col-sm-2 custom-select" size="3">
             <option value="nombre">Nombre</option>
             <option value="marca">Marca</option>
-            <option value="codBarr">Co. Barras</option>
+            <option value="codigo_barras">Co. Barras</option>
         </select>
 
         <input class="col-sm-2 ml-1 btn btn-outline-success" type="submit" value="Buscar">
     </div>
 </form>
-<script>
-    function InvalidMsg(textbox) {
-        if (textbox.value === '') {
-            textbox.setCustomValidity('Por favor ingrese un argumento!');
-        } else {
-            textbox.setCustomValidity('');
-        }
-    }
-</script>
